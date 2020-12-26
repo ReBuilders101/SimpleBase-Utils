3ac696de8a03a3121496694070bb963eec515073
ReBuilders101 <rebuilders101@gmail.com>
2020-12-26 23:55:05 +0100

    Added module-info (name: simplebase.util)

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * @author Lars
+ *
+ */
+module simplebase.util {
+	exports dev.lb.simplebase.util;
+	exports dev.lb.simplebase.util.value;
+	exports dev.lb.simplebase.util.timer;
+	exports dev.lb.simplebase.util.function;
+	exports dev.lb.simplebase.util.annotation;
+	exports dev.lb.simplebase.util.task;
+//	exports debug.simplebase;
+}
